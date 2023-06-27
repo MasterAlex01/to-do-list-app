d97c4fb8a1f1c71a875a57fe2572f02f65ed8fb1
MasterAlex01 <26Alexandra.Anderson@boiseschools.net>
2023-06-27 10:45:21 -0600

    Initial Commit

---
+++ to do list app/to_do_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_list_appApp.swift
+//  to do list app
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct to_do_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
